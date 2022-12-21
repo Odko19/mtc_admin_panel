4801466015368c0c9e31cf92ff96ba2cb40536e9
Odko19 <gerelrosegerel@gmail.com>
2022-12-21 22:59:31 +0800

    Add file

--- back-end/modules/resnum/resnum.service.js
+++ back-end/modules/resnum/resnum.service.js
@@ -1,22 +1,24 @@
-const oracle_db = require("../../db/oracle_db/oracle")
+const oracle_db = require("../../db/oracle_db/oracle");
 
 async function getAllResNum(req) {
   const { page, status, limit } = req.query;
-  console.log(req.query)
+  console.log(req.query);
   const startId = (page - 1) * limit;
-  const data_count = await oracle_db.query("select count(*) from ut_re_num_mst where STATUS='"+ status +"'");
-  const data = await oracle_db.query("select * from ut_re_num_mst where ROWNUM <= '"+limit+"'");
-  const data1 = await oracle_db.query("select * from ut_re_num_mst where ROWNUM <= '"+limit+"'" );
+  console.log(startId);
+  const data_count = await oracle_db.query(
+    "select count(*) from ut_re_num_mst where STATUS='" + status + "'"
+  );
+  const data = await oracle_db.query(
+    "select * from ut_re_num_mst where ROWNUM <= 6"
+  );
+  const data1 = await oracle_db.query("select * from ut_re_num_mst");
   // const data = await oracle_db.query("SELECT * FROM ut_re_num_mst");
-console.log(data)
+  console.log(data1);
   return {
     success: true,
     data,
   };
 }
-
-
-
 
 module.exports = {
   getAllResNum,
