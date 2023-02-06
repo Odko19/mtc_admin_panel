0648811e6a5442f8fd3f6a51616689e70193d6b7
Odko19 <gerelrosegerel@gmail.com>
2023-02-06 17:33:24 +0800

    Add file

--- back-end/modules/authencation/auth.service.js
+++ back-end/modules/authencation/auth.service.js
@@ -1,7 +1,33 @@
 const db = require("../../db/db");
+var axios = require("axios");
 
 async function getLoginUser(req) {
   const { firstName, password } = req.body;
+
+  var data1 = JSON.stringify({
+    user: firstName,
+    password: password,
+  });
+
+  var config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "http://10.0.125.17:88/api/v1/auth/login",
+    headers: {
+      Authorization: "Basic TVRVQjAwNzA0NToxMTExMTE=",
+      "Content-Type": "application/json",
+    },
+    data: data1,
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log("data", response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
   const data = await db.query(
     `select *
     from users where firstName=?`,
--- back-end/modules/payment/payment.service.js
+++ back-end/modules/payment/payment.service.js
@@ -1,14 +1,45 @@
 const oracle_db = require("../../db/oracle_db/oracle");
 
 async function getAllPayment(req) {
-  const data = await oracle_db.query("Select * from MTC_SC_PAYMENT_TRANS ");
-  const data1 = await oracle_db.query(
-    "Select * from MTC_SC_PAYMENT_TRANS_DETAIL"
-  );
-  return {
-    data,
-    data1,
-  };
+  const { page, limit, id } = req.query;
+  if ((page, limit)) {
+    const startId = (page - 1) * limit;
+    const count = await oracle_db.query(
+      "select count(*) as count from MTC_SC_PAYMENT_TRANS "
+    );
+    const totalPage = count[0].COUNT / limit;
+    // const data2 = await oracle_db.query(
+    //   "select MTC_SC_PAYMENT_TRANS.STATUS , MTC_SC_PAYMENT_TRANS.INVOICE_DESC, MTC_SC_PAYMENT_TRANS.AMOUNT, MTC_SC_PAYMENT_TRANS.STATUS, MTC_SC_PAYMENT_TRANS.CREATED_DATE, MTC_SC_PAYMENT_TRANS.UPDATE_DATE, MTC_SC_PAYMENT_TRANS.EMAIL,  MTC_SC_PAYMENT_TRANS.USER_ID, MTC_SC_PAYMENT_TRANS.CARD_TYPE, MTC_SC_PAYMENT_TRANS_DETAIL.PAYMENT_STATUS, MTC_SC_PAYMENT_TRANS_DETAIL.PAYMENT_AMOUNT, MTC_SC_PAYMENT_TRANS_DETAIL.PAYMENT_CURRENCY,MTC_SC_PAYMENT_TRANS_DETAIL.PAYMENT_WALLET,MTC_SC_PAYMENT_TRANS_DETAIL.ACCOUNT_BANK_NAME, MTC_SC_PAYMENT_TRANS_DETAIL.AMOUNT  from MTC_SC_PAYMENT_TRANS INNER JOIN MTC_SC_PAYMENT_TRANS_DETAIL ON MTC_SC_PAYMENT_TRANS.INVOICE_ID = MTC_SC_PAYMENT_TRANS_DETAIL.INVOICE_ID  order by TRANS_ID OFFSET '" +
+    //     startId +
+    //     "' ROWS FETCH NEXT '" +
+    //     limit +
+    //     "' ROWS ONLY "
+    // );
+    const data = await oracle_db.query(
+      "select * from MTC_SC_PAYMENT_TRANS INNER JOIN MTC_SC_PAYMENT_TRANS_DETAIL ON MTC_SC_PAYMENT_TRANS.INVOICE_ID = MTC_SC_PAYMENT_TRANS_DETAIL.INVOICE_ID  order by TRANS_ID OFFSET '" +
+        startId +
+        "' ROWS FETCH NEXT '" +
+        limit +
+        "' ROWS ONLY "
+    );
+    return {
+      totalPages: Math.ceil(totalPage),
+      totalDatas: count[0].count,
+      currentPage: JSON.parse(page),
+      currentPageSize: JSON.parse(limit),
+      data,
+    };
+  }
+  if (id) {
+    const data = await oracle_db.query(
+      "select * from MTC_SC_PAYMENT_TRANS INNER JOIN MTC_SC_PAYMENT_TRANS_DETAIL ON MTC_SC_PAYMENT_TRANS.INVOICE_ID = MTC_SC_PAYMENT_TRANS_DETAIL.INVOICE_ID  WHERE TRANS_ID='" +
+        id +
+        "'"
+    );
+    return {
+      ...data[0],
+    };
+  }
 }
 
 module.exports = {
