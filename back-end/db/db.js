1201dfde76d126f5b76b4f20f9b6fe4788bcfc3f
Odko19 <gerelrosegerel@gmail.com>
2023-04-25 09:00:43 +0800

    Add file

--- back-end/db/db.js
+++ back-end/db/db.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql2");
 const config = require("./config");
 const pool = mysql.createPool(config.db).promise();
-const connection = mysql.createConnection(config.db);
 
 async function query(sql, params, ...args) {
   for (var i = 0; i < args.length; ++i) {
@@ -11,22 +10,4 @@ async function query(sql, params, ...args) {
   return rows;
 }
 
-async function beginTransation() {
-  pool.getConnection((err, connection) => {
-    connection.beginTransation();
-  });
-}
-
-async function roleBack() {
-  return await connection.roleBack();
-}
-
-async function commit() {
-  return await connection.commit();
-}
-
-async function end() {
-  return await connection.end();
-}
-
-module.exports = { query, beginTransation, roleBack, commit, end };
+module.exports = { query };
